bb0af5c9d85c8bc5836a5d0f6911bc45a2a054ec
Prabhav Goyal <prabhav.goyal@gmail.com>
2025-11-16 16:09:36 -0800

    Added modeOverride for shared headers

--- lib/ui/components/shared_h1.tsx
+++ lib/ui/components/shared_h1.tsx
@@ -1,10 +1,15 @@
 import SharedText, { sharedTextProps } from "./shared_text"
 
-export default function SharedH1({ text }: sharedTextProps) {
+export default function SharedH1({ text, mode }: sharedTextProps) {
     const webFontSize = 60
     const mobileFontSize = 40
 
     return (
-        <SharedText text={text} webFontSize={webFontSize} mobileFontSize={mobileFontSize}/>
+        <SharedText 
+            text={text} 
+            webFontSize={webFontSize} 
+            mobileFontSize={mobileFontSize} 
+            modeOverride={mode}
+        />
     )
 }
--- lib/ui/components/shared_h2.tsx
+++ lib/ui/components/shared_h2.tsx
@@ -1,10 +1,15 @@
 import SharedText, { sharedTextProps } from "./shared_text"
 
-export default function SharedH2({ text }: sharedTextProps) {
+export default function SharedH2({ text, mode }: sharedTextProps) {
     const webFontSize = 40
     const mobileFontSize = 30
 
     return (
-        <SharedText text={text} webFontSize={webFontSize} mobileFontSize={mobileFontSize}/>
+        <SharedText 
+            text={text} 
+            webFontSize={webFontSize} 
+            mobileFontSize={mobileFontSize} 
+            modeOverride={mode}
+        />
     )
 }
