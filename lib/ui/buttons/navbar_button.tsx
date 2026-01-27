4480a7bb52e91c27e61787e2e16a1731cb0c467a
cachandlerdev <70250943+cachandlerdev@users.noreply.github.com>
2026-01-26 21:21:09 -0800

    fix: remove navbar redundancy

--- lib/ui/buttons/navbar_button.tsx
+++ lib/ui/buttons/navbar_button.tsx
@@ -17,68 +17,23 @@ export default function NavBarButton({ onPress, imagePath, text, isSelected=fals
     // that would involve some painful infra changes.
     const [hover, setHover] = useState(false)
 
-    // TODO: find a way to do this without css duplication
-    const styles = StyleSheet.create({
-        button: {
-            backgroundColor: Colors.P4,
-            height: 72,
-            width: '100%',
-
-            padding: 12,
-            borderRadius: 10,
-            borderWidth: 3,
-            borderColor: Colors.P1,
-
-            display: 'flex',
-            flexDirection: 'row',
-            alignSelf: 'center',
-            alignItems: 'center',
-            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
-        },
-        buttonHover: {
-            backgroundColor: Colors.P4,
-            height: 72,
-            width: '100%',
-
-            padding: 12,
-            borderRadius: 10,
-            borderWidth: 3,
-            borderColor: Colors.P1,
-
-            display: 'flex',
-            flexDirection: 'row',
-            alignSelf: 'center',
-            alignItems: 'center',
-            boxShadow: 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)'
-        },
-        text: {
-            fontSize: 16,
-            color: Colors.S1,
-            fontWeight: 700,
-            textTransform: 'none',
-            textAlign: "center",
-        },
-        imageStyle: {
-            width: 48,
-            height: 48,
-            objectFit: 'contain',
-            resizeMode: 'contain',
-        },
-        spacer: {
-            flexGrow: 1
-        }
-    });
-
-    const image = (
-        <PlatformImage 
-            imagePath={imagePath}
-            width={48} 
-            height={48}
-            style={styles.imageStyle}/>
+    const navbarContent = (
+        <>
+            <Text style={styles.text}>
+                {text}
+            </Text>
+            <View style={styles.spacer}/>
+            <PlatformImage 
+                imagePath={imagePath}
+                width={48} 
+                height={48}
+                style={styles.imageStyle}/>
+        </>
     )
-    
-    if (isSelected) {
-        return (
+
+    return (
+        <>
+        { isSelected ?
             <TouchableOpacity 
                 onMouseEnter={() => {setHover(true)}}
                 onMouseLeave={() => {setHover(false)}}
@@ -89,37 +44,70 @@ export default function NavBarButton({ onPress, imagePath, text, isSelected=fals
                     end={{x: 1, y: 1}} 
                     colors={[Colors.GGrad1, Colors.GGrad2]}
                     style={styles.buttonHover}>
-                    <Text style={styles.text}>
-                        {text}
-                    </Text>
-                    <View style={styles.spacer}/>
-                    <PlatformImage 
-                        imagePath={imagePath}
-                        width={48} 
-                        height={48}
-                        style={styles.imageStyle}/>
+                    {navbarContent}
                 </LinearGradient>
             </TouchableOpacity>
-        )
-    } else {
-        return (
+        :
             <TouchableOpacity 
                 onMouseEnter={() => {setHover(true)}}
                 onMouseLeave={() => {setHover(false)}}
                 style={hover ? styles.buttonHover : styles.button}
                 onPress={onPress}>
-
-                <Text style={styles.text}>
-                    {text}
-                </Text>
-                <View style={styles.spacer}/>
-                <PlatformImage 
-                    imagePath={imagePath}
-                    width={48} 
-                    height={48}
-                    style={styles.imageStyle}/>
+                {navbarContent}
             </TouchableOpacity>
-        )
-    }
-
+        }
+        </>
+    )
 }
+
+// TODO: find a way to do this without css duplication
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: Colors.P4,
+        height: 72,
+        width: '100%',
+
+        padding: 12,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: Colors.P1,
+
+        display: 'flex',
+        flexDirection: 'row',
+        alignSelf: 'center',
+        alignItems: 'center',
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
+    },
+    buttonHover: {
+        backgroundColor: Colors.P4,
+        height: 72,
+        width: '100%',
+
+        padding: 12,
+        borderRadius: 10,
+        borderWidth: 3,
+        borderColor: Colors.P1,
+
+        display: 'flex',
+        flexDirection: 'row',
+        alignSelf: 'center',
+        alignItems: 'center',
+        boxShadow: 'inset 0px 4px 4px rgba(0, 0, 0, 0.25)'
+    },
+    text: {
+        fontSize: 16,
+        color: Colors.S1,
+        fontWeight: 700,
+        textTransform: 'none',
+        textAlign: "center",
+    },
+    imageStyle: {
+        width: 48,
+        height: 48,
+        objectFit: 'contain',
+        resizeMode: 'contain',
+    },
+    spacer: {
+        flexGrow: 1
+    }
+});
