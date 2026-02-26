b1d9d9fd77b6659f7f5239dbab1da04ce50df2d6
Prabhav Goyal <prabhav.goyal@gmail.com>
2026-02-26 15:09:58 -0800

    Fix: API Bugs for Usage Series & Most Used Devices

--- web/app/api_utils/types.ts
+++ web/app/api_utils/types.ts
@@ -2,4 +2,5 @@ export type { signUpInfo, basicCreds } from 'ui/types'
 export type Result<T> = { ok: true; value: T } | { ok: false, error: string }
 export interface SessionRes { sessionId: string, minutesAlive: number }
 export interface CheckUserbasicCredsRes { valid: boolean, userId: string }
-
+export interface RawUsagePoint { timestamp: string, value: number }
+export interface RawDevicePoint { device_id: number, device_name: string, total_energy: number }
